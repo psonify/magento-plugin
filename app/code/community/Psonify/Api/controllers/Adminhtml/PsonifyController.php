1b56cd3951fbef47ec2f4912470ea1b8c84009eb
MyStream <github@mystream.co.uk>
2015-06-24 18:43:42 +0100

    General Cleanup
    Needs Comments

--- app/code/community/Psonify/Api/controllers/Adminhtml/PsonifyController.php
+++ app/code/community/Psonify/Api/controllers/Adminhtml/PsonifyController.php
@@ -4,16 +4,18 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-class Psonify_Api_Adminhtml_PsonifyController extends Mage_Adminhtml_Controller_Action
-{
-    public function indexAction()
-    {
-        $this->loadLayout()
-            ->_setActiveMenu('psonify')
-            ->_title($this->__('Index Action'));
-        $this->renderLayout();
-    }
- 
-    
+class Psonify_Api_Adminhtml_PsonifyController extends Mage_Adminhtml_Controller_Action {
+
+	/**
+	 * [indexAction description]
+	 * @return [type] [description]
+	 */
+	public function indexAction() {
+		$this->loadLayout()
+			->_setActiveMenu('psonify')
+			->_title($this->__('Index Action'));
+		$this->renderLayout();
+	}
 }
+
 ?>
