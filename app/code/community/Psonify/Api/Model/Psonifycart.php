a6d302443262d6dc822691d1f26cd50024547bf5
MyStream <github@mystream.co.uk>
2015-06-24 18:51:20 +0100

    General Clean Up
    Questions about single underscore.

--- app/code/community/Psonify/Api/Model/Psonifycart.php
+++ app/code/community/Psonify/Api/Model/Psonifycart.php
@@ -4,14 +4,16 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+class Psonify_Api_Model_Psonifycart extends Mage_Core_Model_Abstract {
 
-class Psonify_Api_Model_Psonifycart extends Mage_Core_Model_Abstract
-{
-    public function _construct()
-    {
-        parent::_construct();
-        $this->_init('api/psonifycart');
-    }
+	/**
+	 * [_construct description]
+	 * @return [type] [description]
+	 */
+	public function _construct() {
+		parent::_construct();
+		$this->_init('api/psonifycart');
+	}
 }
 
 ?>
--- app/code/community/Psonify/Api/Model/Psonifycartitem.php
+++ app/code/community/Psonify/Api/Model/Psonifycartitem.php
@@ -4,14 +4,16 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+class Psonify_Api_Model_Psonifycartitem extends Mage_Core_Model_Abstract {
 
-class Psonify_Api_Model_Psonifycartitem extends Mage_Core_Model_Abstract
-{
-    public function _construct()
-    {
-        parent::_construct();
-        $this->_init('api/psonifycartitem');
-    }
+	/**
+	 * [_construct description]
+	 * @return [type] [description]
+	 */
+	public function _construct() {
+		parent::_construct();
+		$this->_init('api/psonifycartitem');
+	}
 }
 
 ?>
