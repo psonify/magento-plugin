475b6dde47187907a15137714f70e3799fedd2b6
Shailesh <rathod.shailesh19@gmail.com>
2015-06-26 23:06:23 +0530

    commentting.

--- app/code/community/Psonify/Api/Model/Mysql4/Psonifycart/Collection.php
+++ app/code/community/Psonify/Api/Model/Mysql4/Psonifycart/Collection.php
@@ -1,16 +1,13 @@
 <?php
 
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Collection class required to fetch all record from the table.
  */
-
 
 class Psonify_Api_Model_Mysql4_Psonifycart_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract
 {
     public function _construct()
     {
-        //parent::__construct();
         $this->_init('api/psonifycart');
     }
 }
