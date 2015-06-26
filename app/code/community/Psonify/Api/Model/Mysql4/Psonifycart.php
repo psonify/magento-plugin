86d8e776927aafee2e2f281fbe1362170843839e
Shailesh <rathod.shailesh19@gmail.com>
2015-06-26 23:04:22 +0530

    commentting.

--- app/code/community/Psonify/Api/Model/Mysql4/Psonifycart.php
+++ app/code/community/Psonify/Api/Model/Mysql4/Psonifycart.php
@@ -1,12 +1,8 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 class Psonify_Api_Model_Mysql4_Psonifycart extends Mage_Core_Model_Mysql4_Abstract
 {
+    //magento model contruct function to define the model defination.
     public function _construct()
     {   
         $this->_init('api/psonifycart', 'id');
