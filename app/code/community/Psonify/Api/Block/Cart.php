f8d1534b9ef58e55b2c4177fe33fcfddc2f70abe
Shailesh <rathod.shailesh19@gmail.com>
2015-06-26 23:00:44 +0530

    commentting.

--- app/code/community/Psonify/Api/Block/Cart.php
+++ app/code/community/Psonify/Api/Block/Cart.php
@@ -6,21 +6,12 @@
 class Psonify_Api_Block_Cart extends Mage_Core_Block_Template {
 
 	/**
-	 * [__construct description]
+	 * Function to set the template when particular block is called.
 	 */
 	public function __construct() {
 		parent::__construct();
 		$this->setTemplate('psonify/abandonedCart.phtml');
 	}
-
-	/**
-	 * [methodBlock description]
-	 * @return [type] [description]
-	 */
-	public function methodBlock() {
-		return 'information about my block.';
-	}
-
 }
 
 ?>
