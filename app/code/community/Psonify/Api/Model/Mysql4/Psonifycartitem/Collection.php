1a2e94bd4e87e315eb168300f7feb6f9da0800f7
Shailesh <rathod.shailesh19@gmail.com>
2015-06-26 23:05:58 +0530

    commentting.

--- app/code/community/Psonify/Api/Model/Mysql4/Psonifycartitem/Collection.php
+++ app/code/community/Psonify/Api/Model/Mysql4/Psonifycartitem/Collection.php
@@ -3,13 +3,10 @@
 /*
  * Collection class required to fetch all record from the table.
  */
-
-
 class Psonify_Api_Model_Mysql4_Psonifycartitem_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract
 {
     public function _construct()
     {
-        //parent::__construct();
         $this->_init('api/psonifycartitem');
     }
 }
